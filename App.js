d31ba6fe70f8ec6928df8214a45037b07b625626
Dan Wilkins <danwilkins2003@gmail.com>
2023-11-13 18:32:18 +0000

    Actually made changes now

--- App.js
+++ App.js
@@ -1,24 +1,8 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Text, View, Button } from "react-native";
-
-function Screen1({ navigation }) {
-    return (
-        <View>
-            <Text>Screen 1</Text>
-            <Button title="Go to screen 2" onPress={() => navigation.navigate("Screen 2")} />
-        </View>
-    );
-}
-
-function Screen2({ navigation }) {
-    return (
-        <View>
-            <Text>Screen 2</Text>
-            <Button title="Go to screen 1" onPress={() => navigation.navigate("Screen 1")} />
-        </View>
-    );
-}
+import SignUpScreen from "./screens/LoginScreens/SignUpScreen";
+import ForgottenPasswordScreen from "./screens/LoginScreens/ForgottenPasswordScreen";
+import LoginScreen from "./screens/LoginScreens/LoginScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -26,8 +10,9 @@ export default function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name="Screen 1" component={Screen1} />
-                <Stack.Screen name="Screen 2" component={Screen2} />
+                <Stack.Screen name="SignUp" component={SignUpScreen} />
+                <Stack.Screen name="Login" component={LoginScreen} />
+                <Stack.Screen name="ForgottenPassword" component={ForgottenPasswordScreen} />
             </Stack.Navigator>
         </NavigationContainer>
     );
