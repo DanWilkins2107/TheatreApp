e4eb63987c1992c3bac96429c2bd1a8bd20387f4
Dan Wilkins <danwilkins2003@gmail.com>
2023-11-16 14:49:59 +0000

    Fully Finished

--- components/UserDashboard/ProductionButton.jsx
+++ components/UserDashboard/ProductionButton.jsx
@@ -1,16 +1,17 @@
 import { View, Text, TouchableOpacity } from "react-native";
 
-export default function ProductionButton(production) {
-    let memberString = production.members;
-    memberString += production.members === 1 ? " member" : " members";
+export default function ProductionButton({ production }) {
+    let memberString = production.no_of_members;
+    memberString += production.no_of_members === 1 ? " member" : " members";
     return (
-        <TouchableOpacity className = "w-max bg-slate-200 flex-col justify-between p-4 h-36 rounded-lg mt-3 border-2 ">
-            <Text className="text-2xl font-extrabold">{production.name}</Text>
+        <TouchableOpacity className="w-max bg-slate-200 flex-col justify-between p-4 h-36 rounded-lg mt-3 border-2 ">
+            <Text className="text-2xl font-extrabold text-ellipsis" numberOfLines={1}>
+                {production.name}
+            </Text>
             <Text className="text-xl font-semibold">{memberString}</Text>
             <View className="flex-row gap-1">
-                <View className="w-10 h-10 rounded-full bg-blue-500"></View>
-                <View className="w-10 h-10 rounded-full bg-red-500"></View>
-                <View className="w-10 h-10 rounded-full bg-orange-500"></View>
+                {production.ids.map((id) => {
+                return <View className="w-10 h-10 rounded-full bg-blue-500" key={id}></View>})}
             </View>
         </TouchableOpacity>
     );
