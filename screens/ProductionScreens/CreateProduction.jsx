e7513d1f8a1d8b750da17c1fea9971a7b8e11e70
Dan Wilkins <danwilkins2003@gmail.com>
2023-11-16 17:50:09 +0000

    Added Production

---
+++ screens/ProductionScreens/CreateProduction.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function CreateProduction({ navigation }) {
+    return (
+        <View>
+            <Text>Create Production</Text>
+        </View>
+    );
+}
---
+++ screens/ProductionScreens/JoinProduction.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function JoinProduction({ navigation }) {
+    return (
+        <View>
+            <Text>JoinProduction</Text>
+        </View>
+    );
+}
