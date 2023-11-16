75261e510a31e6e5603f5715890e3d84afba215a
Dan Wilkins <danwilkins2003@gmail.com>
2023-11-16 17:46:38 +0000

    Mostly Done

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -1,21 +1,22 @@
-import { View, Image } from "react-native";
+import { View, Image, TouchableOpacity } from "react-native";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faHouse, faUser } from "@fortawesome/free-solid-svg-icons";
 
 export default function Header() {
     return (
-        <View className="flex-row justify-between w-[90%] mr-10">
+        <View className="flex-row justify-between w-[90%] mt-[-20] mr-10">
             <View className="flex-1 basis-0 justify-start flex-row">
-                <FontAwesomeIcon icon={faHouse} size={20} />
+                <TouchableOpacity className=" w-10 h-10 rounded-full flex-row align-middle justify-center pt-[7]">
+                    <FontAwesomeIcon icon={faHouse} size={25} />
+                </TouchableOpacity>
             </View>
             <View className="flex-1 basis-0 justify-center flex-row">
-                <Image
-                    className="self-center w-6 h-6"
-                    source={require("../../assets/logo.png")}
-                />
+                <Image className="self-center w-10 h-10 mt-111" source={require("../../assets/logo.png")} />
             </View>
             <View className="flex-1 basis-0 justify-end flex-row">
-                <FontAwesomeIcon icon={faUser} size={20} />
+                <TouchableOpacity className="w-10 h-10 rounded-full flex-row align-middle justify-center pt-2">
+                    <FontAwesomeIcon icon={faUser} size={23} />
+                </TouchableOpacity>
             </View>
         </View>
     );
