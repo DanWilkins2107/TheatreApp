c0e1f2691c513b106a7f6d362c85f7132386a3b0
Dylan Wulfse <dylan@wulfse.com>
2023-11-19 17:44:38 +0000

    implemented profile photo change

--- screens/UserScreens/UserProfile.jsx
+++ screens/UserScreens/UserProfile.jsx
@@ -1,11 +1,14 @@
-import { Button, Text, View } from "react-native";
-import { useState, useEffect } from "react";
-import { signOut } from "firebase/auth";
-import { get, ref, child } from "firebase/database";
-import { firebase_auth, firebase_db } from "../../firebase.config.js";
+import { Button, Text, View, Image, TouchableOpacity, ActivityIndicator } from "react-native";
+import { signOut, updateProfile } from "firebase/auth";
+import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
+import { launchImageLibraryAsync, getMediaLibraryPermissionsAsync } from "expo-image-picker";
+import { firebase_auth, storage } from "../../firebase.config.js";
+import { useState } from "react";
 
 export default function UserProfileScreen({ navigation }) {
     const [userName, setUserName] = useState("");
+    const [profileURL, setProfileURL] = useState(firebase_auth.currentUser.photoURL || null);
+    const [loading, setLoading] = useState(false);
     const auth = firebase_auth;
     const db = firebase_db;
     
@@ -14,6 +17,7 @@ export default function UserProfileScreen({ navigation }) {
             setUserName(snapshot.exists() ? [snapshot.val().firstName, snapshot.val().lastName] : "Anonymous");
         });
     }, []);
+    const storageRef = ref(storage);
 
     const handleSignOut = () => {
         try {
@@ -23,12 +27,80 @@ export default function UserProfileScreen({ navigation }) {
         }
     };
 
+    const handleProfileChange = async () => {
+        launchImageLibraryAsync({ quality: 0.1 }).then((response) => {
+            setLoading(true);
+            if (!response.canceled) {
+                fetch(response.assets[0].uri).then((image) => {
+                    image.blob().then((blob) => {
+                        uploadBytes(
+                            ref(
+                                storageRef,
+                                "images/" +
+                                    auth.currentUser.uid +
+                                    response.assets[0].fileName.substring(
+                                        response.assets[0].fileName.lastIndexOf(".")
+                                    )
+                            ),
+                            blob
+                        ).then((snapshot) => {
+                            getDownloadURL(snapshot.ref)
+                                .then((url) => {
+                                    updateProfile(auth.currentUser, {
+                                        photoURL: url,
+                                    });
+                                    setProfileURL(url);
+                                    setLoading(false);
+                                })
+                                .catch((error) => {
+                                    console.log(error);
+                                });
+                        });
+                    });
+                });
+            }
+        });
+    };
+
     return (
-        <View className="flex-1 mx-10 justify-center">
-            <Text className="text-center text-3xl font-extrabold">
-                Welcome {userName ? `${userName[0]} ${userName[1]}` : ""}
-            </Text>
-            <Button title="Log Out" onPress={handleSignOut} />
+        <View className="flex flex-col h-full">
+            <View className="w-max bg-white flex-row justify-left rounded-3xl px-4 m-2 border-2 align-middle items-center">
+                <TouchableOpacity
+                    className="m-3 rounded-full bg-purple-300"
+                    onPress={() => {
+                        handleProfileChange();
+                    }}
+                >
+                    {loading ? (
+                        <View className="w-28 h-28 rounded-full border-2 items-center justify-center">
+                            <ActivityIndicator size="large" color="#f1f5f9" />
+                        </View>
+                    ) : profileURL ? (
+                        <Image
+                            className="w-28 h-28 rounded-full border-2"
+                            source={{
+                                uri: profileURL,
+                            }}
+                            resizeMode="cover"
+                        />
+                    ) : (
+                        <View className="w-28 h-28 rounded-full border-2 items-center justify-center">
+                            <Text className="font-extrabold text-4xl p-0 m-0">
+                                {auth.currentUser.displayName.split(" ")[0][0]}
+                                {auth.currentUser.displayName.split(" ")[1][0]}
+                            </Text>
+                        </View>
+                    )}
+                </TouchableOpacity>
+                <View className="w-[212] flex-col">
+                    <Text className="ml-4 text-xl font-extrabold text-ellipsis" numberOfLines={1}>
+                        {auth.currentUser.displayName}
+                    </Text>
+                </View>
+            </View>
+            <View className="">
+                <Button title="Log Out" color="red" onPress={handleSignOut} />
+            </View>
         </View>
     );
 }
