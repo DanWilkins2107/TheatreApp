c7c2d5c6f0d9b04a9ef3e0d8163405934b45466f
Dan Wilkins <danwilkins2003@gmail.com>
2024-02-12 14:41:25 +0000

    Framework kinda

---
+++ screens/ProductionScreens/BudgetAddExpense.jsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function BudgetAddExpense({ navigation, route }) {
+    return (
+        <View>
+            <Text>BudgetAddExpense</Text>
+        </View>
+    );
+}
