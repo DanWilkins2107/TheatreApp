ecfde47ec6ab6f984d8666c10599cc9e7b655288
Dan Wilkins <danwilkins2003@gmail.com>
2024-02-20 23:59:37 +0000

    Small PRs from now on please

--- screens/ProductionScreens/BudgetAddExpense.jsx
+++ screens/ProductionScreens/BudgetAddExpense.jsx
@@ -3,31 +3,63 @@ import { useContext, useState } from "react";
 import SmallFormButton from "../../components/Form/SmallFormButton.jsx";
 import { AlertContext } from "../../components/Alert/AlertProvider";
 import { icon } from "@fortawesome/fontawesome-svg-core/import.macro";
-
+import FormField from "../../components/Form/FormField.jsx";
 
 export default function BudgetAddExpense({ navigation, route }) {
     const [budget, setBudget] = useState("");
     const [reference, setReference] = useState("");
+    const [description, setDescription] = useState("");
+    const [recieptURL, setRecieptURL] = useState("");
     const [cost, setCost] = useState("");
     const { setAlert } = useContext(AlertContext);
-    submitForm = () => {
+
+    const submitForm = () => {
         setAlert("Created Budget", "bg-green-500", icon({ name: "circle-check" }));
     };
+
+    const setRecieptLibrary = () => {
+        // TODO: Add Library Reciept Functionality
+    };
+
+    const setRecieptCamera = () => {
+        // TODO: Add Camera Reciept Functionality
+    };
+
     return (
-        <View className="py-2 flex">
+        <View className="py-2 flex justify-center items-center">
             <Text className="self-center text-3xl font-extrabold">Add Expense</Text>
-            <ScrollView className="h-5/6">
+            <ScrollView className="h-5/6 w-10/12">
                 <View>
-                    <Text>Select Budget Section</Text>
+                    {/* TODO: Select Budget */}
+                    <Text className="text-lg font-semibold text-center">Select Budget</Text>
                 </View>
                 <View>
-                    <Text>Reference</Text>
+                    <Text className="text-lg font-semibold text-center">Reference</Text>
+                    <FormField
+                        value={reference}
+                        placeholder="Reference"
+                        onChangeText={setReference}
+                    />
                 </View>
                 <View>
-                    <Text>Cost</Text>
+                    <Text className="text-lg font-semibold text-center">Description</Text>
+                    <FormField
+                        value={description}
+                        placeholder="Description"
+                        onChangeText={setDescription}
+                        multiline
+                    />
                 </View>
                 <View>
-                    <Text>Reciept</Text>
+                    <Text className="text-lg font-semibold text-center">Cost</Text>
+                    <FormField value={cost} placeholder="Cost (£)" onChangeText={setCost} />
+                </View>
+                {/* TODO: Add Reciept Camera/Photo Library Buttons */}
+                {/* TODO: Edit Reciept Implementation */}
+                <View>
+                    {recieptURL ? 
+                    <Text>Reciept Exists</Text> :
+                    <Text>Reciept Doesn't Exist</Text>}
                 </View>
             </ScrollView>
             <View className="flex flex-row w-max justify-around">
