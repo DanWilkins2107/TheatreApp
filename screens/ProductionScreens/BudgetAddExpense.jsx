732769c19eb8446105601437a5d5e6556c130c41
Dan Wilkins <danwilkins2003@gmail.com>
2024-03-04 15:51:42 +0000

    Done it baby

--- screens/ProductionScreens/BudgetAddExpense.jsx
+++ screens/ProductionScreens/BudgetAddExpense.jsx
@@ -9,12 +9,15 @@ import AddRecieptButton from "../../components/Budget/AddRecieptButton.jsx";
 import { FontAwesomeIcon } from "@fortawesome/react-native-fontawesome";
 import { faCamera, faImages } from "@fortawesome/free-solid-svg-icons";
 import { ref, uploadBytes, getDownloadURL } from "firebase/storage";
-import { launchImageLibraryAsync, launchCameraAsync, ImagePicker, useCameraPermissions } from "expo-image-picker";
+import {
+    launchImageLibraryAsync,
+    launchCameraAsync,
+    requestCameraPermissionsAsync,
+} from "expo-image-picker";
 import { storage } from "../../firebase.config.js";
 
 export default function BudgetAddExpense({ navigation, route }) {
     const expenseID = Math.floor(Math.random() * 10 ** 20);
-    const [receiptLoading, setReceiptLoading] = useState(false);
     const [budget, setBudget] = useState("");
     const [reference, setReference] = useState("");
     const [description, setDescription] = useState("");
@@ -28,6 +31,9 @@ export default function BudgetAddExpense({ navigation, route }) {
         // TODO: Complete Submit Form Functionality
     };
 
+    // NOTE TO BE REMOVED!!! Basically what do you reckon is cleaner out of 
+    // setReceiptLibrary and setReceiptCamera? Will change them to be consistent! 
+    // Also do u reckon a upload function is needed?
     const setReceiptLibrary = () => {
         launchImageLibraryAsync({ quality: 0.1 }).then((response) => {
             if (!response.canceled) {
@@ -70,46 +76,40 @@ export default function BudgetAddExpense({ navigation, route }) {
         });
     };
 
-    const setReceiptCamera = () => {
-        launchCameraAsync({ quality: 0.1 }).then((response) => {
-            if (!response.canceled) {
-                try {
-                    fetch(response.assets[0].uri).then((image) => {
-                        image.blob().then((blob) => {
-                            uploadBytes(
-                                ref(
-                                    storageRef,
-                                    "receipts/" +
-                                        expenseID +
-                                        response.assets[0].fileName.substring(
-                                            response.assets[0].fileName.lastIndexOf(".")
-                                        )
-                                ),
-                                blob
-                            ).then((snapshot) => {
-                                getDownloadURL(snapshot.ref)
-                                    .then((url) => {
-                                        setReceiptURL(url);
-                                    })
-                                    .catch((error) => {
-                                        setAlert(
-                                            "Error uploading profile picture",
-                                            "bg-red-500",
-                                            icon({ name: "circle-exclamation" })
-                                        );
-                                    });
-                            });
-                        });
-                    });
-                } catch (error) {
-                    setAlert(
-                        "Error uploading profile picture",
-                        "bg-red-500",
-                        icon({ name: "circle-exclamation" })
-                    );
-                }
+    const setReceiptCamera = async () => {
+        const cameraStatus = await requestCameraPermissionsAsync();
+        if (cameraStatus.status != "granted") {
+            console.log("Camera Permission Denied");
+            return;
+        }
+        const result = await launchCameraAsync({ quality: 0.1 });
+
+        if (!result.canceled) {
+            try {
+                const response = await fetch(result.assets[0].uri);
+                const blob = await response.blob();
+                const extension = result.assets[0].uri.substring(
+                    result.assets[0].uri.lastIndexOf(".")
+                );
+                const snapshot = await uploadBytes(
+                    ref(
+                        storageRef,
+                        "receipts/" +
+                            expenseID +
+                            extension
+                    ),
+                    blob
+                );
+                const url = await getDownloadURL(snapshot.ref);
+                setReceiptURL(url);
+            } catch (error) {
+                setAlert(
+                    "Error uploading receipt",
+                    "bg-red-500",
+                    icon({ name: "circle-exclamation" })
+                );
             }
-        });
+        }
     };
 
     return (
@@ -144,7 +144,7 @@ export default function BudgetAddExpense({ navigation, route }) {
                 </View>
                 <View className="h-80">
                     <Text className="text-lg font-semibold text-center">Receipt</Text>
-                    <ReceiptViewer recieptURL={receiptURL} loading={receiptLoading} />
+                    <ReceiptViewer recieptURL={receiptURL} />
                     <View className="flex-row justify-around mt-[-50]">
                         <AddRecieptButton onPress={setReceiptCamera}>
                             <FontAwesomeIcon icon={faCamera} size={50} />
