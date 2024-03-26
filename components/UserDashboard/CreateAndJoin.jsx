5e00aed1041fa9f6e2203e5d0eaefcf151ae8ecc
Dan Wilkins <danwilkins2003@gmail.com>
2024-03-26 11:00:29 +0000

    Halfway there

--- components/UserDashboard/CreateAndJoin.jsx
+++ components/UserDashboard/CreateAndJoin.jsx
@@ -1,9 +1,10 @@
 import { Text, TouchableOpacity, View } from "react-native";
+import Title from "../TextStyles/Title";
 
 export default function CreateAndJoinButtons(props) {
     return (
         <TouchableOpacity onPress={props.onClick} className="flex-col w-5/12 h-36 justify-around items-center p-3 rounded-lg bg-red-400 border-2">
-            <Text className="text-3xl font-extrabold">{props.title}</Text>
+            <Title>{props.title}</Title>
             <View className="items-center">{props.children}</View>
         </TouchableOpacity>
     );
