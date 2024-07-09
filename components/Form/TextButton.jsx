e1198d6f42fec96b8f571c2b858e82952ce21b2e
Dan Wilkins <danwilkins2003@gmail.com>
2024-07-09 11:06:30 +0100

    Done

--- components/Form/TextButton.jsx
+++ components/Form/TextButton.jsx
@@ -3,7 +3,7 @@ import { TouchableOpacity, Text } from "react-native";
 export default function TextButton({ text, onPress }) {
     return (
         <TouchableOpacity onPress={onPress}>
-            <Text>{message}</Text>
+            <Text className="text-lg text-blue-600 text-center mt-2">{text}</Text>
         </TouchableOpacity>
     );
 }
