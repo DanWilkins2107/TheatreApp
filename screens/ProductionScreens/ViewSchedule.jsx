ee011b95b49a6bdde771f68ae051ed9456fd39be
Dan Wilkins <danwilkins2003@gmail.com>
2024-07-12 13:10:30 +0100

    Done

---
+++ screens/ProductionScreens/ViewSchedule.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export default function ViewScheduleScreen() {
+    return (
+        <View>
+            <Text>View Schedule</Text>
+        </View>
+    );
+}
